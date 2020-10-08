db8dd8825b9ceafc640c93bce7e9b96437df079b
Grabli66 <grabli66@gmail.com>
2020-10-08 21:06:52 +0300

    Экспорт ValueWithCurrency

--- lib/invest_api_dart.dart
+++ lib/invest_api_dart.dart
@@ -5,3 +5,4 @@ export 'package:invest_api_dart/tinkoff_api/tinkoff_portfolio_currency_position.
 export 'package:invest_api_dart/tinkoff_api/tinkoff_candle_data.dart';
 export 'package:invest_api_dart/tinkoff_api/tinkoff_candle_interval.dart';
 export 'package:invest_api_dart/tinkoff_api/tinkoff_rest_api.dart';
+export 'package:invest_api_dart/common/value_with_currency.dart';
